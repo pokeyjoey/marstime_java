b0e930488dc1f0db03a9e03a23f231a215263071
Jeff Roche <jroche@UbuntuDesktop.attlocal.net>
2018-02-04 15:01:38 -0500

    Commit of initial code for MArsDate directory structure and code.

---
+++ source/com/acalendarformars/MarsDate.java
@@ -0,0 +1,17 @@
+/**
+* This program calculates the current data on Mars according to a 
+* calendar invented by Dr. Robert Zubrin.
+*
+* @author Jeff Roche
+* @version 1.0
+* @since 2018-02-04
+*/
+package com.acalendarformars;
+
+public class MarsDate {
+    
+    public MarsDate () {
+        System.out.println("Get your Ass to Mars !!");
+    }
+
+}
---
+++ source/com/acalendarformars/MarsDateTest.java
@@ -0,0 +1,8 @@
+package com.acalendarformars;
+
+public class MarsDateTest {
+
+    public static void main(String[] args) {
+        MarsDate marsDate = new MarsDate();
+    }
+}
