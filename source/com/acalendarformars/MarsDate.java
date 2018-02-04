1182c8a67b594889ca9a671c7fb0f8469eb0d70c
Jeff Roche <jroche@UbuntuDesktop.attlocal.net>
2018-02-04 17:49:14 -0500

    Using TreeMap to produce a sorted key value pair of day of year and month of year (day of year is the last day of the martion month)

--- source/com/acalendarformars/MarsDate.java
+++ source/com/acalendarformars/MarsDate.java
@@ -8,10 +8,37 @@
 */
 package com.acalendarformars;
 
+import java.util.TreeMap;
+
 public class MarsDate {
     
+    private int SOLS_IN_YEAR = 669;
+    private TreeMap<Integer, String> monthsOfMars = new TreeMap<Integer, String>();
+
     public MarsDate () {
         System.out.println("Get your Ass to Mars !!");
+        initializeMonthsOfMars();
     }
+
+    private void initializeMonthsOfMars() {
+        monthsOfMars.put(61, "Gemini");
+        monthsOfMars.put(126, "Cancer");
+        monthsOfMars.put(192, "Leo");
+        monthsOfMars.put(257, "Virgo");
+        monthsOfMars.put(317, "Libra");
+        monthsOfMars.put(317, "Libra");
+        monthsOfMars.put(371, "Scorpius");
+        monthsOfMars.put(421, "Sagittarius");
+        monthsOfMars.put(468, "Capricorn");
+        monthsOfMars.put(514, "Aquarius");
+        monthsOfMars.put(562, "Pisces");
+        monthsOfMars.put(613, "Aries");
+        monthsOfMars.put(669, "Taurus");
+    }
+
+    public TreeMap<Integer, String> getMonthsOfMars() {
+        return monthsOfMars;
+    }
+
 
 }
--- source/com/acalendarformars/MarsDateTest.java
+++ source/com/acalendarformars/MarsDateTest.java
@@ -1,8 +1,14 @@
 package com.acalendarformars;
 
+import java.util.TreeMap;
+
 public class MarsDateTest {
 
     public static void main(String[] args) {
         MarsDate marsDate = new MarsDate();
+
+        // Get the Months of Mars
+        TreeMap<Integer, String> monthsOfMars = marsDate.getMonthsOfMars();
+        System.out.println(monthsOfMars); 
     }
 }
